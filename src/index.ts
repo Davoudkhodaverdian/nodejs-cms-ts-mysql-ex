f5f5737fbd8866528e07fc0dddc05e8f92532b47
Davoudkhodaverdian <davoudkhodaverdian20@gmail.com>
2024-03-22 13:42:59 +0330

    modify cookie

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import homeRouter from './app/routes';
 import listen from './app/setup/listen';
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import cookieParser from 'cookie-parser';
 
 // set dotenv
 dotenv.config({ path: '.env' }); // for process.env
@@ -16,7 +16,7 @@ const app : Express = express();
 // this is for body start
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cookieParser("mySecretkey"));
 // parse requests of content-type - application/json
 // app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
